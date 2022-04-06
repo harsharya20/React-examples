6f5b230f0fef1694601ec26976a3a5b73c8f7154
Harsh <harsharya7002@gmail.com>
2022-04-06 16:28:57 +0530

    event on form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,135 @@
-import React, { createContext } from 'react'
-import CompA from './CompA'
+import React, { useState } from 'react'
 
-
-const FirstName = createContext()
-const LastName = createContext()
 const App = () => {
+  const [inputs,setInputs] =useState()
+  const [fullName,setfullName] = useState()
+  const inputEvent = (event) => {
+    setInputs(event.target.value)
+  }
+
+   const onSubmit = () => {
+     setfullName(inputs)
+   }
   return (
     <>
-  <FirstName.Provider value={'Harsh'}>
-    <LastName.Provider value={'Arya'}>
-      <CompA /> 
-      </LastName.Provider>
-  </FirstName.Provider>
-  </>
+    <h1>Hello {fullName}</h1>
+    <input type="text" placeholder='Enter Your Name' onChange={inputEvent} value={inputs}/>
+    <button onClick={onSubmit }>Click Me</button>
+    </>
   )
 }
 
 export default App
-export { FirstName }
-export { LastName }
+
+
+
+
+
+
+
+// import React from "react";
+// import { useState } from "react";
+
+
+// const colors = {
+//    fontSize: '200px'
+//   }
+// const App = () => {
+
+// let time = new Date().toLocaleTimeString();
+
+// const [ctime, setCtime ] = useState(time)
+
+// const UpdateTime = () => {
+//   time = new Date().toLocaleTimeString();
+//   setCtime(time)
+// }
+
+// setInterval(UpdateTime,1000);
+//   return (
+//     <div style={colors}>{ctime}</div>
+//   )
+// }
+
+// export default App
+
+
+
+
+// const App = () => {
+//   const [count, setCount ] = useState(0)
+//   const Inc = () => {
+//     setCount(count + 1)
+//   }
+
+//   return (
+//     <>
+//     <h1>{count}</h1>
+//     <button onClick={Inc}>Click</button>
+   
+//     </>
+//   )
+// }
+
+// export default App
+
+// const colors = {
+//  display : 'block',
+//  margin : '20px',
+//  marginTop : '200px',
+//  marginLeft: '-130px'
+// }
+// const App = () => {
+
+//   const [time, setTime] = useState(new Date().toLocaleTimeString());
+  
+//   const curtime = () => {
+//     setTime(new Date().toLocaleTimeString())
+//   }
+//   return (
+//     <>
+//     <h1>{time}</h1>
+//     <button style = {colors} onClick={curtime}>Click</button>
+    
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { createContext } from 'react'
+// import CompA from './CompA'
+
+
+// const FirstName = createContext()
+// const LastName = createContext()
+// const App = () => {
+//   return (
+//     <>
+//   <FirstName.Provider value={'Harsh'}>
+//     <LastName.Provider value={'Arya'}>
+//       <CompA /> 
+//       </LastName.Provider>
+//   </FirstName.Provider>
+//   </>
+//   )
+// }
+
+// export default App
+// export { FirstName }
+// export { LastName }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,40 @@
 import React from 'react'
 import ReactDOM  from 'react-dom'
 import './index.css'
+import App from './App'
 
-let currtime= ' '
-const curtime = new Date(2022,4,4,23).getHours();
-const times = {};
-if(curtime >=1 && curtime <12){
-  currtime = 'Good Morning'
-  times.color = 'Orange'
-} else{
-  if(curtime >=12 && curtime <19){
-  times.color = 'purple'
-  currtime = 'Good Afternoon'
-  } else {
-    currtime = 'Good Night'
-    times.color = 'blue'
-  }
-}
+ReactDOM.render(<App />, document.getElementById('root'))
 
 
 
-ReactDOM.render(
-  <>
-  <div>
-  <h1>Hello User, <span style={times}>{currtime}</span> </h1>
-  </div>
-  </>,
-  document.getElementById('root')
-)
+
+
+// let currtime= ' '
+// const curtime = new Date().getHours();
+// const times = {};
+// if(curtime >=1 && curtime <12){
+//   currtime = 'Good Morning'
+//   times.color = 'Orange'
+// } else{
+//   if(curtime >=12 && curtime <19){
+//   times.color = 'purple'
+//   currtime = 'Good Afternoon'
+//   } else {
+//     currtime = 'Good Night'
+//     times.color = 'blue'
+//   }
+// }
+
+
+
+// ReactDOM.render(
+//   <>
+//   <div>
+//   <h1>Hello User, <span style={times}>{currtime}</span> </h1>
+//   </div>
+//   </>,
+//   document.getElementById('root')
+// )
 
 // const name = 'harsh';
 // const img1 = "https://picsum.photos/200/300"
